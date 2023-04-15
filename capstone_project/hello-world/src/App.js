6c4981bbb61e741a678096149ea8c89eb1ba2e2d
alexander-gc <alexandergcca@gmail.com>
2023-04-15 03:06:45 -0600

    dependencies added

--- capstone_project/hello-world/src/App.js
+++ capstone_project/hello-world/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
+        <p>Hello World!</p>
       </header>
     </div>
   );
